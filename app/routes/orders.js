15eabc9066a83c429f54ab38b419898bafb9617d
Jonathan Share <jonny@sharebear.co.uk>
2017-06-05 23:21:02 +0200

    refactor: move list logic and rendering to the index sub-route
    This removes the list from the common code used in the template for both the index and the detail views.

--- app/routes/orders.js
+++ app/routes/orders.js
@@ -1,10 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-    return Ember.$.getJSON("http://localhost:4567/ordermanagement/orders")
-    .then(function(data) {
-      return data.data;
-    });
-  }
 });
--- app/routes/orders/index.js
+++ app/routes/orders/index.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return Ember.$.getJSON("http://localhost:4567/ordermanagement/orders")
+    .then(function(data) {
+      return data.data;
+    });
+  }
 });
