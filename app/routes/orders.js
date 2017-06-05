cbabc8165e5c3b840ba2f0c4bd2888162352dbc7
Jonathan Share <jonny@sharebear.co.uk>
2017-06-05 21:41:56 +0200

    feat: generated an adapter for orders
    This includes the output from `ember generate adapter orders`

--- app/routes/orders.js
+++ app/routes/orders.js
@@ -2,35 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return [
-      {
-      "orderId": "4d11faf4-e287-4339-9f25-5786b1630ea9",
-      "version": 1,
-      "name": "Jonathan Share",
-      "phoneNumber": "98989898",
-      "email": "jonny@sharebear.co.uk",
-      "fromAddress": "Someplace",
-      "toAddress": "Someplace else",
-      "services": [
-        "PACKING"
-      ],
-      "executionDate": "2017-04-01",
-      "comments": null
-    },
-    {
-      "orderId": "c4c988e3-5536-437c-8ce5-695207b7b5e5",
-      "version": 1,
-      "name": "Another customer",
-      "phoneNumber": "98989898",
-      "email": "jonny@sharebear.co.uk",
-      "fromAddress": "Someplace",
-      "toAddress": "Someplace else",
-      "services": [
-        "PACKING"
-      ],
-      "executionDate": "2017-04-01",
-      "comments": null
-    }
-    ]
+    return this.get('store').findAll('order');
   }
 });
