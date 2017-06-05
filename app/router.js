573a272231421adffac0440d7b552f39f1c115b4
Jonathan Share <jonny@sharebear.co.uk>
2017-06-05 22:58:08 +0200

    feat: add route for editing an order
    Code generated from `ember generate route orders/edit`

--- app/router.js
+++ app/router.js
@@ -7,7 +7,9 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('orders');
+  this.route('orders', function() {
+    this.route('edit');
+  });
 });
 
 export default Router;
