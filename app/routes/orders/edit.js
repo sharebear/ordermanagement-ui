bbb7006b7c7acab657995a9a1544d19760a3e465
Jonathan Share <jonny@sharebear.co.uk>
2017-06-06 00:45:53 +0200

    feat: added a simple form field and button for saving order
    This would update the order if the endpoint existed. That's going to have to wait for tomorrow though.

--- app/routes/orders/edit.js
+++ app/routes/orders/edit.js
@@ -6,5 +6,18 @@ export default Ember.Route.extend({
     .then(function(data) {
       return data.data;
     });
+  },
+  actions: {
+    saveOrder() {
+      let current = this.modelFor(this.routeName);
+      Ember.$.ajax(`http://localhost:4567/ordermanagement/orders/${current.orderId}/update-order`, {
+        data: JSON.stringify({ data: current }),
+        contentType: "application/json",
+        method: "POST"
+      })
+      .done(function() {
+        this.refresh();
+      });
+    }
   }
 });
