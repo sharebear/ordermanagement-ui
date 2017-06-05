996ea2025a9b452f80576753f73e482d7732f314
Jonathan Share <jonny@sharebear.co.uk>
2017-06-05 23:16:01 +0200

    feat: add nested route for editing orders
    Trivial rendering for now until I have fixed a small issue with the nesting.

--- app/router.js
+++ app/router.js
@@ -8,7 +8,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('orders', function() {
-    this.route('edit');
+    this.route('edit', { path: '/:orderId'});
   });
 });
 
--- app/routes/orders/edit.js
+++ app/routes/orders/edit.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params) {
+    return Ember.$.getJSON(`http://localhost:4567/ordermanagement/orders/${params.orderId}`)
+    .then(function(data) {
+      return data.data;
+    });
+  }
 });
