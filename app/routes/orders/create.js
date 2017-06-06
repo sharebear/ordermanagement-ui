50eb18fea254186e0fea814d6f66f02dea5f5b58
Jonathan Share <jonny@sharebear.co.uk>
2017-06-06 02:04:21 +0200

    feat: attempt at pretty form fields

--- app/routes/orders/create.js
+++ app/routes/orders/create.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return {};
+  },
+  actions: {
+    saveOrder() {
+      console.log(this.modelFor(this.routeName));
+    }
+  }
 });
